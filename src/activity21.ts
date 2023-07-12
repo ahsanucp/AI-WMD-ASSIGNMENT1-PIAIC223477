c0e3fd5b2e3333fa73bf76a7f43c90cdaa8d7401
Ahsan Ullah Khan <ahsanullah303@gmail.com>
2023-07-13 04:28:06 +0500

    feat: add activity no 21

---
+++ src/activity21.ts
@@ -0,0 +1,67 @@
+export const Activity21 = () => {
+  interface Mountain {
+    name: string;
+    height: number;
+  }
+
+  interface River {
+      name: string;
+      length: number;
+  }
+
+  interface Country {
+      name: string;
+      capital: string;
+  }
+
+  interface City {
+      name: string;
+      population: number;
+  }
+
+  interface Language {
+      name: string;
+      speakers: number;
+  }
+
+  function createObjects() {
+    const objects: {
+        mountains: Mountain[];
+        rivers: River[];
+        countries: Country[];
+        cities: City[];
+        languages: Language[];
+    } = {
+        mountains: [
+            { name: "Karakoram", height: 8611 },
+            { name: "Himalayas", height: 8848 },
+        ],
+        rivers: [
+            { name: "Indus", length: 2900 },
+            { name: "Jhelum", length: 725 },
+        ],
+        countries: [
+            { name: "Pakistan", capital: "Islamabad" },
+            { name: "India", capital: "New Delhi" },
+        ],
+        cities: [
+            { name: "Karachi", population: 14910352 },
+            { name: "Lahore", population: 11126285 },
+        ],
+        languages: [
+            { name: "Urdu", speakers: 100000000 },
+            { name: "Punjabi", speakers: 90000000 },
+        ],
+    };
+
+    return objects;
+  }
+
+  const myObjects = createObjects();
+
+  console.log(myObjects.mountains);
+  console.log(myObjects.rivers);
+  console.log(myObjects.countries);
+  console.log(myObjects.cities);
+  console.log(myObjects.languages);
+}
