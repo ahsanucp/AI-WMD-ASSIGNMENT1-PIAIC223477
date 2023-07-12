358896112ce3d965d1957ff617008cbd5daeda58
Ahsan Ullah Khan <ahsanullah303@gmail.com>
2023-07-13 04:28:35 +0500

    feat: add activity no 26

---
+++ src/activity26.ts
@@ -0,0 +1,16 @@
+export const Activity26 = () => {
+  let alien_color = 'green';
+
+  if (alien_color === 'green') {
+      console.log("Congratulations! You just earned 5 points for shooting the alien.");
+  } else {
+      console.log("Congratulations! You just earned 10 points for shooting the alien.");
+  }
+  
+  alien_color = 'red';
+  if (alien_color === 'green') {
+      console.log("Congratulations! You just earned 5 points for shooting the alien.");
+  } else {
+      console.log("Congratulations! You just earned 10 points for shooting the alien.");
+  }
+}
