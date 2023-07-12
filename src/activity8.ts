8351a2b727f322beae43fa335580d04d659b6a6d
Ahsan Ullah Khan <ahsanullah303@gmail.com>
2023-07-13 04:26:45 +0500

    feat: add activity no 8

---
+++ src/activity8.ts
@@ -0,0 +1,11 @@
+export const Activity8 = () => {
+  let add = 6 + 2;
+  let sub = 16 - 8;
+  let mul = 2 * 4;
+  let div = 48 / 6;
+
+  console.log(`5 + 3 = ${add}`);
+  console.log(`12 - 4= ${sub}`);
+  console.log(`4 * 2= ${mul}`);
+  console.log(`32 / 4= ${div}`);
+}
