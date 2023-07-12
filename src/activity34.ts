7a5ac73680f05bef3b395fd5d9fb104aef0b96fb
Ahsan Ullah Khan <ahsanullah303@gmail.com>
2023-07-13 04:29:24 +0500

    feat: add activity no 34

---
+++ src/activity34.ts
@@ -0,0 +1,9 @@
+export const Activity34 = () => {
+  let pizzaNames: string[] = ['Pepperoni', 'Fajita', 'Supreme'];
+
+  for (let i = 0; i < pizzaNames.length; i++) {
+    console.log("I like " + pizzaNames[i] + " pizza.");
+  }
+  
+  console.log("I really love pizza!");
+}
