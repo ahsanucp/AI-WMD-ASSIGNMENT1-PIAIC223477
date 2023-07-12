00d1e13fa763daa1cf33c4ed9a20d9b977ee123d
Ahsan Ullah Khan <ahsanullah303@gmail.com>
2023-07-13 04:26:19 +0500

    feat: add activity no 4

---
+++ src/activity4.ts
@@ -0,0 +1,6 @@
+export const Activity4 = () => {
+  const quote: string = "Life is like a box of chocolates. You never know what you're gonna get.";
+  const author: string = "Forrest Gump";
+  
+  console.log(`${author} once said, "${quote}"`);
+}
