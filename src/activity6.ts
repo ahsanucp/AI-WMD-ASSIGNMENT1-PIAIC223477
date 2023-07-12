6832b1c15892c54150b4f037abdfe04489e672c7
Ahsan Ullah Khan <ahsanullah303@gmail.com>
2023-07-13 04:26:32 +0500

    feat: add activity no 6

---
+++ src/activity6.ts
@@ -0,0 +1,6 @@
+export const Activity6 = () => {
+  let name: string = "\t   \n  Ahsan Ullah Khan   \t   \n";
+  console.log("Original name:", name);
+  let strippedName: string = name.trim();
+  console.log("Stripped name:", strippedName);
+}
