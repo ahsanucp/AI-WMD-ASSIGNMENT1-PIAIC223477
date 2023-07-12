cd381a4323a438946f1610f1645df442881ddc1c
Ahsan Ullah Khan <ahsanullah303@gmail.com>
2023-07-13 04:25:59 +0500

    feat: add activity no 2

---
+++ src/activity2.ts
@@ -0,0 +1,5 @@
+export const Activity2 = () => {
+  const personName: string = "Ahsan";
+  const message: string = `Hello ${personName}, would you like to learn some Python today?`;
+  console.log(message);
+}
